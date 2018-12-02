2a3d4abd4cc8727cf20cbf15be11270a3c772f50
SwordsGays <572231346@qq.com>
2018-12-02 18:11:28 +0800

    虚拟滑杆

---
+++ Assets/Script/PlayerMover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+    public GameObject sliderblock;
+    private float r;
+    public Transform initPos;
+    private bool inpress;
+    private int touchID;
+    private Vector2 clickPos;
+    public Camera uicamera;
+    private Vector2 movePos;
+    private void Awake()
+    {
+        
+        r = this.GetComponent<UISprite>().width / 2;
+    }
+
+
+    void OnPress(bool pressed)
+    {
+        inpress = pressed;
+        touchID = UICamera.currentTouchID;
+    }
+
+    private void Update()
+    {
+        
+        if (inpress)
+        {
+            //触碰的点的位置
+            clickPos = UICamera.GetTouch(touchID,false).pos;
+            //虚拟按键的点的屏幕坐标
+            Vector2 JoyStick = new Vector2(uicamera.WorldToScreenPoint(this.transform.position).x, uicamera.WorldToScreenPoint(this.transform.position).y);
+            if (Vector3.Distance(JoyStick,clickPos) <= r)
+            {
+                sliderblock.transform.position = UICamera.currentCamera.ScreenToWorldPoint(clickPos);
+            }
+            else
+            {
+                sliderblock.transform.localPosition = (clickPos-JoyStick).normalized * r;
+            }
+            
+        }
+        else
+        {
+            sliderblock.transform.localPosition = initPos.localPosition;
+        }
+    }
+
+
+
+}
