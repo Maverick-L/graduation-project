192db788e9548708d1e4601a8eb13abe6302f57c
SwordsGays <572231346@qq.com>
2018-12-06 13:32:31 +0800

    视角旋转

--- Assets/NGUI/Examples/Scripts/Other/SpinWithMouse.cs
+++ Assets/NGUI/Examples/Scripts/Other/SpinWithMouse.cs
@@ -16,11 +16,16 @@ public class SpinWithMouse : MonoBehaviour
 	void OnDrag (Vector2 delta)
 	{
 		UICamera.currentTouch.clickNotification = UICamera.ClickNotification.None;
-
+        print(delta);
 		if (target != null)
 		{
-			target.localRotation = Quaternion.Euler(0f, -0.5f * delta.x * speed, 0f) * target.localRotation;
-		}
+           // if (-2f < delta.y &&delta.y< 2f)
+         //   {
+                target.localRotation = Quaternion.Euler(0f, -0.5f * delta.x * speed, 0f) * target.localRotation;
+        //    }
+         //   else if(-2f < delta.x && delta.y < 2f) 
+                target.localRotation = Quaternion.Euler( -0.5f * delta.y * speed,0f, 0f) * target.localRotation;
+        }
 		else
 		{
 			mTrans.localRotation = Quaternion.Euler(0f, -0.5f * delta.x * speed, 0f) * mTrans.localRotation;
---
+++ Assets/Script/ViewMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewMove : MonoBehaviour {
+    public GameObject Maincamera;
+    private int touchId=0;
+    public float speed = 2f;
+
+
+    void OnDrag(Vector2 delet)
+    {
+        print("1");
+    }
+
+
+}
