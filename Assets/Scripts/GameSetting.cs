1cf42acbef936980654d07a87e4ae012cf4759dd
lihaixia <412862430@qq.com>
2018-12-20 15:00:56 +0800

    st

---
+++ Assets/Scripts/GameSetting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetting : MonoBehaviour {
+
+	public float volume = 1;
+    public GameObject startUI;
+    public GameObject mainUI;
+	public void OnVolumeChanged() {
+		volume = UIProgressBar.current.value;//调节声音大小
+	}
+
+	//设置画面切换
+	public TweenPosition startPanelTween;
+	public TweenPosition optionPanleTween;
+
+
+	public void OnOptionButtonClick() {
+		startPanelTween.PlayForward();
+		optionPanleTween.PlayForward();
+	}
+
+	public void OnCompleteSettingButtonClick() {
+		startPanelTween.PlayReverse();
+		optionPanleTween.PlayReverse();
+	}
+    public void OnPlayButtonClick()
+    {
+        startUI.SetActive(false);
+        mainUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/TestTextList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTextList : MonoBehaviour {
+
+	private UITextList textList;
+
+	// Use this for initialization
+	void Start () {
+		textList = this.GetComponent<UITextList>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
