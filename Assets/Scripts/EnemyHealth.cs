34da911332f4a43cf3242180b315f79261975934
SwordsGays <572231346@qq.com>
2018-12-23 17:04:08 +0800

    添加死亡

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -9,6 +9,11 @@ public class EnemyHealth : MonoBehaviour {
     public float rightAttack = 10f;
     public float footAttack = 30f;
 
+    Animator ani;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
     private void OnEnable()
     {
         //订阅事件
@@ -20,6 +25,16 @@ public class EnemyHealth : MonoBehaviour {
         if(AttackName== "Object001")
         {
             print(name + "攻击了" + AttackName);
+            //进行攻击扣血表示
+
+            if (blood <= 0)
+            {
+                ani.SetBool("isDie", true);
+                //死亡之后三秒后消失
+
+                Destroy(this.gameObject, 3f);
+                
+            }
         }
     }
 
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,7 +7,13 @@ public class PlayerHealth : MonoBehaviour {
 
     public float leftAttack = 20f;
     public float rightAttack = 10f;
-    
+
+    Animator ani;
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+    }
+
     private void OnEnable()
     {
         //订阅事件
@@ -19,6 +25,14 @@ public class PlayerHealth : MonoBehaviour {
         if (AttackName == "char_ethan")
         {
             print(name + "攻击了" + AttackName);
+            //进行攻击扣血表示
+
+            if (blood <= 0)
+            {
+                ani.SetBool("isDie", true);
+                //死亡后禁止移动
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;//禁止所有方向上的移动
+            }
         }
     }
 
