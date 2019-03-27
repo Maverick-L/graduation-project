12585567da9f39caad2afa3371677a925ab6d09c
kacokeko <1040973930@qq.com>
2019-03-27 17:03:16 +0800

    UI 3-27 17

--- Assets/Script/Const/Myconst.cs
+++ Assets/Script/Const/Myconst.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Myconts : MonoBehaviour
 {
+<<<<<<< HEAD
     public const string RSOURCE_PREFABS_PATH = "Prefabs";
     public const string RSOURCE_AUDIOCLIP_PATH = "AudioClip";
 	public const string REOURCE_UI_PATH = "UI/";
+=======
+    public const string RSOURCE_PREFABS_PATH = "Prefabs/";
+    public const string RSOURCE_AUDIOCLIP_PATH = "AudioClip/";
+>>>>>>> 3484643dbe10bcf95d980765585a37d3720bf198
 }
--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Test : MonoBehaviour
 {
-    public GameObject panel;
-    // Start is called before the first frame update
-    void Start()
+    public Transform[] t;//存储位置
+    private void Awake()
     {
+        print(t[0].name);
+      //  MainManager._instance._poolManager.Create(go, GameManagers.Type.NPC);
+        StartCoroutine(CreatEnemy());
+
     }
 
-   
+
+    IEnumerator CreatEnemy()
+    {
+        foreach (Transform go in t)
+        {
+            //1.可以使用go.name找到怪物的名字然后把怪物取出来
+            GameObject g = Resources.Load(Myconts.RSOURCE_PREFABS_PATH + go.name) as GameObject;
+            //2.生成GameObject
+            MainManager._instance._poolManager.Create(g, GameManagers.Type.NPC);
+        }
+        yield return 0;
+    }
+
 }
